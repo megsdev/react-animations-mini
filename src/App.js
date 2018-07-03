d8017f45a3129b5c19f067a71065338af9926443
Megan Duclos <37349288+megsdev@users.noreply.github.com>
2018-07-03 13:03:53 -0600

    finished

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Card from './Card';
 import './App.css';
+import { TransitionMotion, spring } from 'react-motion'
 
 
 export default class App extends Component {
@@ -55,17 +56,33 @@ export default class App extends Component {
         })
     }
 
+    getDefaultStyles = () => {
+        return this.state.todos.map( todo => {
+            return Object.assign({}, todo, {style: {height: 0, opacity: 0 }})
+        })
+    }
+
+    getStyles = () => {
+        return this.state.todos.map( todo => {
+            return Object.assign({}, todo, {style: {height: spring(65), opacity: spring(1)}})
+        })
+    }
+
+    willEnter = () => {
+        return {
+            height: 0,
+            opacity: 0
+        }
+    }
+
+    willLeave = () => {
+        return {
+            height: spring(0),
+            opacity: spring(0)
+        }
+    }
 
     render() {
-
-        const todos = this.state.todos.map( (todo, i) => {
-            return <Card 
-                        key={i}
-                        toggle={ this.toggle }
-                        removeTodo={ this.removeTodo } 
-                        todo={ todo } /> 
-        })
-
         return(
             <div className='app'>
                 <h1>to-dos</h1>
@@ -80,9 +97,27 @@ export default class App extends Component {
                                 /> 
                         </form>   
                     </div>
-                    <div>
-                        { todos }
-                    </div>  
+                    <TransitionMotion 
+                        defaultStyles={ this.getDefaultStyles() }
+                        styles={ this.getStyles() } 
+                        willEnter={ this.willEnter }
+                        willLeave={ this.willLeave }
+                        >
+                        {(styles) => {
+                            console.log({styles: styles})
+                            return (
+                                <div>
+                                    {styles.map( (todo) => {
+                                        return <Card 
+                                            key={ todo.key }
+                                            toggle={ this.toggle }
+                                            removeTodo={ this.removeTodo } 
+                                            todo={ todo } /> 
+                                    })}
+                                </div>  
+                            )
+                        }}
+                    </TransitionMotion>
                 </div> 
             </div> 
         )
